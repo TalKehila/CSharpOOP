0e6fa99d8d2f57436d1d28753f62481986f1182e
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 13:08:42 +0300

    Example of Dispose and Using

---
+++ Disposable&using/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (A a = new A())
+            {
+
+            }
+        }
+    }
+
+    class A : IDisposable
+    {
+        B b = new B();
+        D d = new D();
+        public void Dispose()
+        {
+            b.Dispose();
+            d.Dispose();
+            Console.WriteLine("Disp A");
+        }
+    }
+
+    class B : IDisposable
+    {
+        C c = new C();
+        public void Dispose()
+        {
+            c.Dispose();
+            Console.WriteLine("Disp B");
+        }
+    }
+
+    class C : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disp C");
+        }
+    }
+
+    class D : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disp D");
+        }
+    }
+}
