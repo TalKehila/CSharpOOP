ba9c2fe0f37dd407c5d6db9d716fb6b2f05bd9be
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 13:10:30 +0300

    Indexer Bus Example

---
+++ Indexer/INdexerEXpmle/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class BusArray
+{
+    private Bus[] buses;
+
+    public Bus this[int i]
+    {
+        get { return buses[i]; }
+        set { buses[i] = value; }
+    }
+
+    public BusArray()
+    {
+        buses = new Bus[20000];
+    }
+}
+
+internal class Bus
+{
+}
